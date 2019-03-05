a8024be51e0f132143764f5a298b65daaae7d4f5
Leo Preissegger <preissegger.leo@a1.net>
2019-03-05 14:21:44 +0100

    reading token from header

--- api/src/Api/JWTManagement.php
+++ api/src/Api/JWTManagement.php
@@ -6,11 +6,11 @@ use Firebase\JWT\JWT;
 
 class JWTManagement
 {
-    static public function generateToken() {
+    static public function generateToken(int $userId): string {
 
     }
 
     static function verifyToken(): ?string {
-
+        return $_SERVER['HTTP_AUTHORIZATION'];
     }
 }
